0ec77e5dc67faf0df05ee50d8a5c9070672a6a65
valentin.pean <valentin.pean@bts-malraux.net>
2014-09-24 13:59:42 +0000

    mise à jour

--- initiation_argparse.py
+++ initiation_argparse.py
@@ -9,16 +9,23 @@ parser.add_argument("nomFichierPlaylist", help="nom du fichier de sortie")
 parser.add_argument("--titrePlaylist", help="titre choisis")
 parser.add_argument("--artistePlaylist", help="nom de l'artiste")
 parser.add_argument("--albumPlaylist", help="album présent dans la playlist")
-parser.add_argument("--genrePlaylist", help="genre%pourcentage")
-parser.add_argument("--sousGenrePlaylist", help="genre%pourcentage")
+parser.add_argument("--genrePlaylist", help="genre%pourcentage", nargs = 2)
+parser.add_argument("--sousGenrePlaylist", help="sousgenre%pourcentage", nargs = 2)
 
 args = parser.parse_args()
 
-print("duree : " + str(args.dureePlaylist))
-print("format : " + args.formatPlaylist)
-print("nom du fichier : " +args.nomFichierPlaylist)
-print("titres choisis : " +args.titrePlaylist)
-print("artistes choisis : " +args.artistePlaylist)
-print("album choisis : " +args.albumPlaylist)
-print("genre choisis : " +args.genrePlaylist)
-print("sous genre choisis : " +args.sousGenrePlaylist)
+def verification_Qte(quantite):
+    return int(quantite)
+
+args.genrePlaylist[1] = verification_Qte(args.genrePlaylist[1])
+
+print(args)
+
+# print("duree : " + str(args.dureePlaylist))
+# print("format : " + args.formatPlaylist)
+# print("nom du fichier : " +args.nomFichierPlaylist)
+# print("titres choisis : " +args.titrePlaylist)
+# print("artistes choisis : " +args.artistePlaylist)
+# print("album choisis : " +args.albumPlaylist)
+# print("genre choisis : " +args.genrePlaylist[0] + args.genrePlaylist[1])
+# print("sous genre choisis : " +args.sousGenrePlaylist)
