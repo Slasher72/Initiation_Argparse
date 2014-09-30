3552efa86be9b9f7fb2e7ea1e2d8fe39ef06750f
valentin.pean <valentin.pean@bts-malraux.net>
2014-09-30 15:15:57 +0000

    modif logging avec ajout de logging.info et logging.error

--- initiation_argparse.py
+++ initiation_argparse.py
@@ -1,5 +1,8 @@
 import argparse
 import logging
+import sys
+
+logging.basicConfig(filename = "journal_log.log", level = logging.DEBUG)
 
 parser = argparse.ArgumentParser()
 
@@ -16,21 +19,27 @@ parser.add_argument("--sousGenrePlaylist", help="sousgenre%pourcentage", nargs =
 
 args = parser.parse_args()
 
+logging.debug(args)
+
 def validerQuantite(quantite):
     try:
+        logging.info("début du programme de verif")
         good = int(quantite)
+        if good <= 0:
+            raise Exception('UnderGround')
         return good
+        logging.info("La saisie est correct")
     except ValueError:
-        print("La valeur saisie n'est pas un entier : '" + quantite + "'")
+        logging.error("La valeur saisie pour la quantité n'est pas une valeur numérique : '" + quantite + "'")
         exit(1)
+    except Exception as err:
+        if err.args[0] == 'UnderGround':
+            logging.error("La valeur saisie pour la quantité ne peut être négative : '%i'" % genre)
+            exit(1)
 
 args.genrePlaylist[1] = validerQuantite(args.genrePlaylist[1])
 
 
-if args.genrePlaylist[1] > 0 and args.genrePlaylist[1] <= 100:
-    print(args)
-else:
-    print("le pourcentage doit être compris entre 0 et 100")
 
 
 
